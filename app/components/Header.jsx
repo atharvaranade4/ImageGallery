61ba51898a6137061fa9c48395d2aed6ad418412
Atharva Ranade <atharvaranade4@gmail.com>
2024-04-13 00:51:34 -0700

    setup google auth

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,13 @@
+"use client"
 import Image from 'next/image'
+import { useSession, signIn, signOut } from "next-auth/react"
 import React from 'react'
 import { HiSearch, HiBell, HiChat } from "react-icons/hi";
 
+
 function Header() {
+  const { data: session } = useSession()
+  console.log(session)
   return (
     <div className='flex gap-3 md:gap-2 items-center p-6'>
       <Image
@@ -29,13 +34,20 @@ function Header() {
         <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
         <HiBell className='text-[40px] text-gray-500'/>
         <HiChat className='text-[40px] text-gray-500'/>
+        { session?.user? 
         <Image 
-          src="/logo.jpg" 
+          src={session.user.image} 
           alt="user-image" 
           width={50} 
           height={50} 
           className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'
-        />
+        /> : 
+          <button 
+          className='font-semibold p-2 px-4 rounded-full'
+          onClick={() => signIn()}>
+            Login
+          </button>
+        }
     </div>
   )
 }
